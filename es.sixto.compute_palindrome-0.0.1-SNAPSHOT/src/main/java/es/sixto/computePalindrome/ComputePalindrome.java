e1a1c29d6e01bc3afb07e38e093997782738369d
sixto <sixto.cantolla@gmail.com>
2013-07-08 10:21:16 +0200

    Create test compute palindrome, RED

--- es.sixto.compute_palindrome-0.0.1-SNAPSHOT/src/main/java/es/sixto/computePalindrome/ComputePalindrome.java
+++ es.sixto.compute_palindrome-0.0.1-SNAPSHOT/src/main/java/es/sixto/computePalindrome/ComputePalindrome.java
@@ -17,8 +17,21 @@ public class ComputePalindrome {
 
 	}
 
-	public boolean isPalindrome(int i) {
-		return true;
+	public boolean isPalindrome(int intToValidate) {
+		int reversed = 0;
+		int rest = 0;
+		int tempNumber = intToValidate;
+		while (tempNumber>0) {
+			rest = tempNumber%10;
+			reversed = reversed*10+rest;
+			tempNumber = tempNumber/10;
+		}
+		return intToValidate==reversed;
+	}
+
+	public int computePalindrome(int i) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
--- es.sixto.compute_palindrome-0.0.1-SNAPSHOT/src/test/java/es/sixto/computePalindrome/ComputePalindromeTest.java
+++ es.sixto.compute_palindrome-0.0.1-SNAPSHOT/src/test/java/es/sixto/computePalindrome/ComputePalindromeTest.java
@@ -10,7 +10,21 @@ public class ComputePalindromeTest {
 	public void testIsPalindromo() {
 		ComputePalindrome validatePalindrome = new ComputePalindrome();
 		boolean result = validatePalindrome.isPalindrome(121);
+		assertEquals(true, result);
 	}
 
+	@Test
+	public void testIsPalindromoFail() {
+		ComputePalindrome validatePalindrome = new ComputePalindrome();
+		boolean result = validatePalindrome.isPalindrome(122);
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void testComputePalindrome() {
+		ComputePalindrome validatePalindrome = new ComputePalindrome();
+		int result = validatePalindrome.computePalindrome(195);
+		assertEquals(9339, result);
+	}
 
 }
