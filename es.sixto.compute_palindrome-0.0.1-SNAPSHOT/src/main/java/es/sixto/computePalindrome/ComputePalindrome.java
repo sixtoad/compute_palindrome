71ce799106a68b30300343f1ac27b0295ebf69a0
sixto <sixto.cantolla@gmail.com>
2013-07-08 09:53:26 +0200

    Initial commit with structure

---
+++ es.sixto.compute_palindrome-0.0.1-SNAPSHOT/src/main/java/es/sixto/computePalindrome/ComputePalindrome.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package es.sixto.computePalindrome;
+
+/**
+ * @author Sixto
+ *
+ */
+public class ComputePalindrome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ es.sixto.compute_palindrome-0.0.1-SNAPSHOT/src/test/java/es/sixto/computePalindrome/ComputePalindromeTest.java
@@ -0,0 +1,14 @@
+package es.sixto.computePalindrome;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComputePalindromeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
